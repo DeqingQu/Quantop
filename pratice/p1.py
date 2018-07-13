222ac8c02c132a9af4bbbd3a18cdd7a33428ab3d
DeqingQu <qu.deqing@gmail.com>
2018-07-12 17:21:58 -0700

    tf variable usage

--- pratice/p1.py
+++ pratice/p1.py
@@ -11,3 +11,25 @@ with tf.Session() as sess:
     result = sess.run(product)
     print(result)
 
+# variable
+
+state = tf.Variable(0, name='counter')
+
+# 定义常量 one
+one = tf.constant(1)
+
+# 定义加法步骤 (注: 此步并没有直接计算)
+new_value = tf.add(state, one)
+
+# 将 State 更新成 new_value
+update = tf.assign(state, new_value)
+
+# 如果定义 Variable, 就一定要 initialize
+init = tf.global_variables_initializer()  # 替换成这样就好
+
+# 使用 Session
+with tf.Session() as sess:
+    sess.run(init)
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
