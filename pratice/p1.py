e82066efc957dee4b95b773672ee74e06b9ffb77
DeqingQu <qu.deqing@gmail.com>
2018-07-12 18:20:08 -0700

    tensorflow placeholder usage

--- pratice/p1.py
+++ pratice/p1.py
@@ -11,8 +11,9 @@ with tf.Session() as sess:
     result = sess.run(product)
     print(result)
 
-# variable
 
+
+# variable
 state = tf.Variable(0, name='counter')
 
 # 定义常量 one
@@ -33,3 +34,14 @@ with tf.Session() as sess:
     for _ in range(3):
         sess.run(update)
         print(sess.run(state))
+
+#   How to use placeholder
+# 在 Tensorflow 中需要定义 placeholder 的 type ，一般为 float32 形式
+input1 = tf.placeholder(tf.float32)
+input2 = tf.placeholder(tf.float32)
+
+# mul = multiply 是将input1和input2 做乘法运算，并输出为 output
+ouput = tf.multiply(input1, input2)
+
+with tf.Session() as sess:
+    print(sess.run(ouput, feed_dict={input1: [7.], input2: [2.]}))
