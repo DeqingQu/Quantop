edf9a1ab04317bad2fe582411b5af7eee0de16fd
DeqingQu <qu.deqing@gmail.com>
2018-07-14 13:01:28 -0700

    plot nn practice

--- pratice/p2.py
+++ pratice/p2.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
@@ -33,7 +34,23 @@ init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(x_data, y_data)
+    plt.ion()
+    plt.show()
+
     for i in range(1000):
         sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
         if i % 50 == 0:
+            try:
+                ax.lines.remove(lines[0])
+            except Exception:
+                pass
+            prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+            # plot the prediction
+            lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+            plt.pause(0.1)
             print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+
