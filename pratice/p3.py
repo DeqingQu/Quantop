851688994fa71ed4e51c12e36c13e15bfac8cd92
DeqingQu <qu.deqing@gmail.com>
2018-07-15 21:23:17 -0700

    tensor board 2

--- pratice/p3.py
+++ pratice/p3.py
@@ -3,20 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
-    # define layer name
-    with tf.name_scope('layer'):
-        # define weights name
-        with tf.name_scope('weights'):
-            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
-        with tf.name_scope('biases'):
-            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
-        with tf.name_scope('Wx_plus_b'):
-            Wx_plus_b = tf.matmul(inputs, Weights) + biases
-        if activation_function is None:
-            outputs = Wx_plus_b
-        else:
-            outputs = activation_function(Wx_plus_b)
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
+    # define a new var
+    layer_name = 'layer%s' % n_layer
+    with tf.name_scope(layer_name):
+        #  define layer name
+        with tf.name_scope('layer'):
+            # define weights name
+            with tf.name_scope('weights'):
+                Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+                tf.summary.histogram(layer_name + '/weights', Weights)
+            with tf.name_scope('biases'):
+                biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+                tf.summary.histogram(layer_name + '/biases', biases)
+            with tf.name_scope('Wx_plus_b'):
+                Wx_plus_b = tf.matmul(inputs, Weights) + biases
+            if activation_function is None:
+                outputs = Wx_plus_b
+            else:
+                outputs = activation_function(Wx_plus_b)
+            tf.summary.histogram(layer_name + '/outputs', outputs)  # Tensorflow >= 0.12
         return outputs
 
 
@@ -29,13 +35,14 @@ with tf.name_scope('inputs'):
     ys = tf.placeholder(tf.float32, [None, 1], name='y_in')
 
 with tf.name_scope('l1'):
-    l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+    l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
 
 with tf.name_scope('prediction'):
-    prediction = add_layer(l1, 10, 1, activation_function=None)
+    prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
 
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+    tf.summary.scalar('loss', loss)
 
 with tf.name_scope('train'):
     train_step = tf.train.AdamOptimizer(0.1).minimize(loss)
@@ -44,6 +51,8 @@ init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
+
+    merged = tf.summary.merge_all()  # tensorflow >= 0.12
 
     writer = tf.summary.FileWriter("logs/", sess.graph)
 
@@ -65,4 +74,7 @@ with tf.Session() as sess:
             lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
             plt.pause(0.1)
             print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+            # tensor board
+            rs = sess.run(merged, feed_dict={xs: x_data, ys: y_data})
+            writer.add_summary(rs, i)
 
