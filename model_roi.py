d52913a893e8912335bf92bcb3e4302a2d85a3a3
DeqingQu <qu.deqing@gmail.com>
2018-09-03 21:10:49 -0700

    add hedging to roi calculation

--- model_roi.py
+++ model_roi.py
@@ -11,9 +11,11 @@ class ROICalculator(object):
     def __load_pred(self, stock_symbol):
         return np.loadtxt("data/" + stock_symbol + "_pred.txt", delimiter=',')
 
-    def calculate_roi(self, top_num):
+    def calculate_roi(self, top_num, with_hedging=True):
 
         pred_res, truth_res = {}, {}
+        stock_data = StockDataSet("^GSPC", input_size=1, num_steps=30, test_ratio=0.05)
+        sp500 = stock_data.test_y
 
         for stock_symbol in self.stock_symbols:
             pred_res[stock_symbol] = self.__load_pred(stock_symbol)
@@ -27,13 +29,13 @@ class ROICalculator(object):
             for k, v in pred_res.items():
                 d[k] = v[i]
             sorted_d = sorted(d.items(), key=lambda item: item[1], reverse=True)
-            print(sorted_d)
             new_roi = 0.0
             for j in range(top_num):
                 key = sorted_d[j][0]
                 new_roi += roi / top_num * (1.0 + truth_res[key][j])
-                print(truth_res[key][j])
             roi = new_roi
+            if with_hedging:
+                roi -= sp500[i]
             print("echo %d : %f" % (i, roi))
         return roi
 
